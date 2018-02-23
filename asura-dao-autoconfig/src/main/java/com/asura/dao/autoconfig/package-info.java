624ce6d0dda82fb4123aef37553b7fcaa77fc60a
liusq23 <liusq23@ziroom.com>
2018-02-23 15:05:51 +0800

    init

---
+++ asura-dao-autoconfig/src/main/java/com/asura/dao/autoconfig/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @FileName: package-info.java
+ * @Package: com.asura.dao.autoconfig
+ * @author liusq23
+ * @created 2018/2/23 下午2:59
+ * <p>
+ * Copyright 2018 asura
+ */
+package com.asura.dao.autoconfig;
---
+++ asura-dao-core/src/main/java/com/asura/dao/core/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @FileName: package-info.java
+ * @Package: com.asura.dao
+ * @author liusq23
+ * @created 2018/2/23 下午2:58
+ * <p>
+ * Copyright 2018 asura
+ */
+package com.asura.dao.core;
